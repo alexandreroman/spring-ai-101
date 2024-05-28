1f9a835368f466583aac01f5b48bcfdbac345f3f
Alexandre Roman <alexandre.roman@broadcom.com>
2024-05-28 12:47:39 +0200

    Add support for Mistral AI

---
+++ src/main/java/com/broadcom/tanzu/demos/springai101/MistralAIConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024 Broadcom, Inc. or its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.broadcom.tanzu.demos.springai101;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.mistralai.MistralAiChatModel;
+import org.springframework.ai.mistralai.MistralAiEmbeddingModel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(name = "app.ai-provider", havingValue = "mistralai", matchIfMissing = true)
+class MistralAIConfig {
+    @Bean
+    ChatClient.Builder chatClientBuilder(MistralAiChatModel mistralAiChatModel) {
+        return ChatClient.builder(mistralAiChatModel);
+    }
+
+    @Bean
+    @Primary
+    EmbeddingModel embeddingModel(MistralAiEmbeddingModel mistralAiEmbeddingModel) {
+        return mistralAiEmbeddingModel;
+    }
+}
